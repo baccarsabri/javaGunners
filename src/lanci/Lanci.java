e85ff801cb82da2c2e1fcd79fcf00d9f5f4a1b02
yasssam97 <100165386+yasssam97@users.noreply.github.com>
2022-04-12 00:57:41 +0200

    main

--- src/lanci/Lanci.java
+++ src/lanci/Lanci.java
@@ -7,10 +7,12 @@ package lanci;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.mail.MessagingException;
 import models.catégorie;
 import models.contrat;
 import services.ServiceCatégorie;
 import services.ServiceContrat;
+import utils.EmailSender;
 
 /**
  *
@@ -21,29 +23,39 @@ public class Lanci {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MessagingException {
         // TODO code application logic here
-        
+       
          ServiceCatégorie cat= new ServiceCatégorie();
          ServiceContrat ca= new ServiceContrat();
-         
-         catégorie res=new catégorie("yass");
+          
+       // AJOUT CATÉGORIE 
+         /*
+         catégorie res=new catégorie("yassIIII");
 
         
          java.util.Date date = new java.util.Date();
          java.sql.Date sqlDate = new java.sql.Date(date.getTime()); 
          
-         contrat aj=new contrat(1,1,77,sqlDate,"a");
+         contrat aj=new contrat(1,1,5555,sqlDate,"KJJJJJJ");
          contrat up=new contrat(4445,1,1,77,sqlDate,"pending");
-      
-        ca.createContrat(aj);
-        ca.updatecontrat(up);
+   
+       //  AJOUT CONTRAT
+           ca.createContrat(aj);
+       // UPDATE CONTRAT
+            ca.updatecontrat(up);
+       // DELETE CONTRAAT
+          ca.Deletecontrat(4473);
+
         
         
-        
-         catégorie upp=new catégorie(4457,"sam");
-         cat.updateCatégorie(upp);
-         cat.DeleteCatégorie(4462);
+           catégorie upp=new catégorie(4457,"sam");
+        // UPDATE CATÉGORIE
+
+            cat.updateCatégorie(upp);
+        // DELETE CATÉGORIE 
+
+             cat.DeleteCatégorie(4462);
 
       
       //affiche catégorie  
@@ -57,10 +69,66 @@ public class Lanci {
         List<contrat> contrats = new ArrayList();
         contrats = ca.readcontrats(); 
         System.out.println(contrats.toString()); 
+       
+       */
+          
+         
+        
+      //  EMAIL
+        EmailSender.sendEmailWithAttachments("samaali.mohamedyassine@esprit.tn", "test CONTRAT ", "message");
         
         
+        /*
+       // affiche par statut
+           List<contrat> contrats = new ArrayList();
+           contrats=ca.RechercherparStatut("a");
+           System.out.println(contrats.toString()); 
+        */     
+
+       /* 
+      //  prix total de contrat
+                float total=0;
+                 total=ca.Sommetotale();
+                 System.out.println(total);
+    */
+       
+       
+        /*         
+       // TRI PRIX  
+                 List<contrat> yassine = new ArrayList();
+                 yassine=ca.TriPrix();
+                 System.out.println(yassine);
+        */
         
-        
+       
+              
+
+      // Tri par nom 
+      /*
+        List<catégorie> catégories = new ArrayList();
+        catégories=cat.TriNom();
+        System.out.println(catégories);
+*/
+      
+      
+
+     // Recherche
+       /*  List<catégorie> recherche = new ArrayList();
+        recherche=cat.RechercherparNom("a");
+        System.out.println(recherche);
+     */  
+       //getbyid
+       /*
+       catégorie catt=new catégorie();
+       catt=cat.GetbyID(4473);
+        System.out.println(catt);
+      
+        contrat ct= new contrat();
+        ct=ca.GetbyID(4450);
+        System.out.println(ct);
+              
+        */
+       
     }
     
 }
